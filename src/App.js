f3e45b81d855408cc6c85d0b1e50c93ebfea42d2
Amy Steel <amylouisesteel@hotmail.co.uk>
2022-09-22 20:09:34 +0100

    Initial setup
    Initial setup using create react app
    
    Co-Authored-By: yasminejpj <108760156+yasminejpj@users.noreply.github.com>
    Co-Authored-By: Muhammad Faizan <74507860+mfaizan22@users.noreply.github.com>

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Fruit from "./Fruit";
+
+function App() {
+  return (
+    <div className="App">
+      <Fruit />
+    </div>
+  );
+}
+
+export default App;
